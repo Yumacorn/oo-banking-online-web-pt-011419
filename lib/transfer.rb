084ecd44cadcc812e575719f6ffb008515ccc258
Ryan Mui <reachryanmui@gmail.com>
2019-03-03 17:12:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
   def execute_transaction
     # binding.pry
     "Transaction rejected. Please check your account balance" if !self.valid?
+
     if @status == "pending" && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.deposit(@amount)
